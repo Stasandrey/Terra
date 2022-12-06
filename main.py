019aa780ca95385629b6af1e4510d835d15313d2
stasandrey <staschenyuk.andrey@gmail.com>
2022-12-06 15:24:56 +0300

    Отрефакторил. В рабочих модулях PEP не ругается.

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ port = serial.Serial(port=PORT, baudrate=SPEED)
 print(port.readline())
 lcd = Lcd(port)
 for i in range(1000):
-    lcd.cursor_to(2,0)
+    lcd.cursor_to(2, 0)
 
     lcd.print(str(i))
--- modbus.py
+++ modbus.py
@@ -6,32 +6,32 @@ import os
 import random
 
 from paho.mqtt import client as mqtt_client
-def portSetup():
-    instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)  # port name, slave address (in decimal)
-    instrument.serial.port = '/dev/ttyUSB0'                    # this is the serial port name
+
+
+def port_setup():
+    instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
+    instrument.serial.port = '/dev/ttyUSB0'
     instrument.serial.baudrate = 115200         # Baud
     instrument.serial.bytesize = 8
-    instrument.serial.parity   = serial.PARITY_NONE
+    instrument.serial.parity = serial.PARITY_NONE
     instrument.serial.stopbits = 1
-    instrument.serial.timeout  = 0.5        # seconds
+    instrument.serial.timeout = 0.5        # seconds
 
     instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
     instrument.clear_buffers_before_each_transaction = True
     print(instrument)
     return instrument
 
-def read( pribor ):
-    ## Read temperature (PV = ProcessValue) ##
+
+def read(pribor):
+    # Read temperature (PV = ProcessValue) ##
     temperature = pribor.read_register(30019, 0, 4)  # Registernumber, number of decimals
     print(temperature/10)
     return temperature/10
 
+
 result = os.system('echo baziliy | sudo -S chmod 666 /dev/ttyUSB0')
 
-
-# echo
-#     password | sudo - S
-#     команда
 
 broker = '144.21.37.118'
 port = 1883
@@ -40,37 +40,41 @@ client_id = f'python-mqtt-{random.randint(0, 1000)}'
 username = 'ubuntu'
 password = 'baziliy'
 
+
 def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+    # def on_connect(_, a, b, rc):
+    #     a = 0
+    #     b = 0
+    #     _ = a + b
+    #     if rc == 0:
+    #         print("Connected to MQTT Broker!")
+    #     else:
+    #         print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
-    client.on_connect = on_connect
+    # client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
 
 def publish(client, t):
-        msg = t
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
+    msg = t
+    client.publish(topic, msg)
+
+    # result: [0, 1]
+    # status = result[0]
 
 
 def run():
 
     client = connect_mqtt()
     client.loop_start()
-    pribor = portSetup()
+    pribor = port_setup()
     while True:
-#
         temp = read(pribor)
         publish(client, temp)
         time.sleep(1)
-## Change temperature setpoint (SP) ##
+# Change temperature setpoint (SP) ##
 # NEW_TEMPERATURE = 95
-# instrument.write_register(24, NEW_TEMPERATURE, 1)  # Registernumber, value, number of decimals for storage
+# instrument.write_register(24, NEW_TEMPERATURE, 1)
