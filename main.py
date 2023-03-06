ab93ff0660b5b8a588fdd463262334e04f20776c
andrey <staschenyuk.andrey@gmail.com>
2023-03-06 12:51:35 +0300

    Начинаем переход на UART.

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ print(data.objects)
 print(data.tags)
 
 print(data.values)
-# PORT = "/dev/ttyUSB0"
-# SPEED = 115200
-# result = os.system('echo baziliy | sudo -S chmod 666 %s' % PORT)
-# port = serial.Serial(port=PORT, baudrate=SPEED)
-# print(port.readline())
+PORT = "/dev/ttyUSB0"
+SPEED = 115200
+result = os.system('echo baziliy | sudo -S chmod 666 %s' % PORT)
+port = serial.Serial(port=PORT, baudrate=SPEED)
+print(port.readline())
 
 
-# console = Console("conf/screen.yaml", port, data)
-# console.run(console.head)
+console = Console("conf/screen.yaml", port, data)
+console.run(console.head)
